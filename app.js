886ecf7181974436650f1079a90ef566b6a2d11c
BlessingAyoola <156815594+BlessingAyoola@users.noreply.github.com>
2024-04-24 20:08:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-const getElement = (selector) => {
-  const element = document.querySelector(selector)
-
- 
-
-const links = getElement('.nav-links')
-
-}
-
 const date = getElement('#date')
 const currentYear = new Date().getFullYear()
 date.textContent = currentYear
